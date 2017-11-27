a73efc81b37d63fde27838862bf38d7aafff0233
Александр Корниенко <kornsasha89@gmail.com>
2017-11-27 14:13:25 +0200

    Изменение данных покупателя в заказе

--- www/catalog/controller/checkout/confirm.php
+++ www/catalog/controller/checkout/confirm.php
@@ -123,10 +123,18 @@ class ControllerCheckoutConfirm extends Controller {
 
 				$order_data['customer_id'] = $this->customer->getId();
 				$order_data['customer_group_id'] = $customer_info['customer_group_id'];
-				$order_data['firstname'] = $customer_info['firstname'];
-				$order_data['lastname'] = $customer_info['lastname'];
-				$order_data['email'] = $customer_info['email'];
-				$order_data['telephone'] = $customer_info['telephone'];
+                if (empty($this->session->data['artPersonalData'])) {
+                    $order_data['firstname'] = $customer_info['firstname'];
+                    $order_data['lastname'] = $customer_info['lastname'];
+                    $order_data['email'] = $customer_info['email'];
+                    $order_data['telephone'] = $customer_info['telephone'];
+                } else {
+                    $order_data['firstname'] = $this->session->data['artPersonalData']['firstname'];
+                    $order_data['lastname'] = $this->session->data['artPersonalData']['lastname'];
+                    $order_data['email'] = $this->session->data['artPersonalData']['email'];
+                    $order_data['telephone'] = $this->session->data['artPersonalData']['telephone'];
+                }
+                unset($this->session->data['artPersonalData']);
 				$order_data['fax'] = $customer_info['fax'];
 				$order_data['custom_field'] = json_decode($customer_info['custom_field'], true);
 			} elseif (isset($this->session->data['guest'])) {
